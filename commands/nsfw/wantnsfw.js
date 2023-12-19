79abb15588f69fcceea3f460d0d30104a1b6c3f8
universopack.com <dksiao352@gmail.com>
2023-12-19 14:16:13 -0600

    actualizando nsfw

--- commands/nsfw/wantnsfw.js
+++ commands/nsfw/wantnsfw.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ChatInputCommandInteraction } = require("discord.js")
+const { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } = require("discord.js")
 const want = require("wantnsfw")
 
 module.exports = {
@@ -11,6 +11,13 @@ module.exports = {
          * @param {ChatInputCommandInteraction} interaction 
          */
 	async execute(interaction) {
+        if (!interaction.channel.nsfw) {
+            const embed = new EmbedBuilder()
+                .setDescription("❌**Este comando solo se puede usar en canales NSFW.**")
+                .setColor("Red")
+                .setImage("https://media.discordapp.net/attachments/1016879071940067488/1054885163592585277/unknown.png");
+            return await interaction.reply({ embeds: [embed] });
+        }
 		const wantnsfw = await want.wantapiwaifu()
         console.log(wantnsfw)
 
